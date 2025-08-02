36fc492d98044e59395f2a7f2246680cd2d5b205
softlife-engineer <abdullahiabonde22@gmail.com>
2025-08-02 23:39:02 +0100

    Updated app

--- App.js
+++ App.js
@@ -1,10 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableOpacity  } from 'react-native';
+import React from 'react';
 
 export default function App() {
+  const [count, setCount] = React.useState(0);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.CountText}>Count:{count} </Text>
+
+      <TouchableOpacity
+        onPress={() => setCount(count + 1)}
+        style={{
+          marginBottom: 20,
+          backgroundColor: "#4CAF50",
+          padding: 10,
+          borderRadius: 20,
+        }}
+      >
+        <Text style={{ color: "#fff", fontSize: 18 }}>Increase</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={() => setCount(count - 1)}
+        style={{
+          marginBottom: 20,
+          backgroundColor: "#4CAF50",
+          padding: 10,
+          borderRadius: 20,
+        }}
+      >
+        <Text style={{ color: "#fff", fontSize: 18 }}>Decrease</Text>
+      </TouchableOpacity>
+      
+      <TouchableOpacity
+        onPress={() => setCount(0)}
+        style={{
+          backgroundColor: "#f44336",
+          padding: 10,
+          borderRadius: 5,
+        }}
+      >
+        <Text style={{ color: "#fff", fontSize: 18 }}>Reset</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +51,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ff6347',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  
+  CountText: {
+    color: "#fff",  
+    fontSize: 20,
+  }
+
 });
---
+++ README.md
@@ -0,0 +1,3 @@
+# Counter App
+
+### This is simple Counter App UI built with React native.
